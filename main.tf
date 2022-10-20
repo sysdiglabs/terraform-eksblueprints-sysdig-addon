cb739668ca62d78dcea4d4becf3ef974b4c00624
manuelbcd <manuel.boira@sysdig.com>
2022-10-20 10:29:52 +0200

    Fixed main.tf and added .tfvars to .gitignore

--- main.tf
+++ main.tf
@@ -3,11 +3,9 @@ module "helm_addon" {
   #Change before publishing
   source               = "github.com/manuelbcd/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.12.2"
   #source               = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.1.0"
-  #
-  manage_via_gitops    = var.manage_via_gitops
-  # TODO cook set_values and set_sensitive_values to register the endpoints, ports and tokens
-  # set_values           = local.set_values
-  # set_sensitive_values = local.set_sensitive_values
-  helm_config          = local.helm_config
-  addon_context        = var.addon_context
+  
+
+  addon_context = var.addon_context
+  set_values    = local.set_values
+  helm_config   = local.helm_config
 }
