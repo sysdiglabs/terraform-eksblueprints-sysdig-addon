f3877488a06fa4cfdc49e74e22f920772ff141c1
manuelbcd <manuel.boira@sysdig.com>
2022-10-20 08:18:51 +0200

    Switched to aws-eks-bluaprints version 4.12.2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Helm addon
 module "helm_addon" {
   #Change before publishing
-  source               = "github.com/manuelbcd/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.1.0"
+  source               = "github.com/manuelbcd/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.12.2"
   #source               = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.1.0"
   #
   manage_via_gitops    = var.manage_via_gitops
