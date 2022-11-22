16c452af0c4a6870df9cc11f47ddc54f2a1ac7f8
manuelbcd <manuel.boira@sysdig.com>
2022-11-22 16:28:27 +0100

    Fix linter errors

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 # Helm addon
 module "helm_addon" {
   #Change before publishing
-  source               = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
+  source        = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon"
+  version       = "2.7.1"
 
   addon_context = var.addon_context
   set_values    = local.set_values
