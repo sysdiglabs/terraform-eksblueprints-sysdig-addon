87efaa4f84432b2c424b2b039c98922dca4755fb
manuelbcd <manuel.boira@sysdig.com>
2022-11-24 12:25:38 +0100

    Versions fix

--- blueprints/getting-started/main.tf
+++ blueprints/getting-started/main.tf
@@ -126,7 +126,7 @@ module "eks_blueprints" {
 }
 
 module "eks_blueprints_kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.17.0"
 
   eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
