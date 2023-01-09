dff4fd9a7aef30249a20e95dcfb7595ef1e298c2
manuelbcd <manuel.boira@sysdig.com>
2023-01-10 00:56:38 +0100

    Update helm provider version to 2.8.0

--- blueprints/lab-security/versions.tf
+++ blueprints/lab-security/versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = ">= 3.8.0"
     }
     random = {
       source  = "hashicorp/random"
