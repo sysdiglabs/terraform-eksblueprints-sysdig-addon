2ffde2528361663e394e146441462c89cdc5425e
manuelbcd <manuel.boira@sysdig.com>
2023-01-17 13:12:50 +0100

    Add monitor-alerts blueprint

---
+++ blueprints/monitor-alerts/versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.8.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.0"
+    }
+    sysdig = {
+      source = "sysdiglabs/sysdig"
+      version = ">=0.5"
+    }
+  }
+}
