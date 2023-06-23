a442e1feac6d3760a48d5d340eabaa5290098d3e
manuelbcd <manuel.boira@sysdig.com>
2023-06-23 16:21:29 +0200

    Fixed version from monitor blueprint

--- blueprints/monitor-alerts/main.tf
+++ blueprints/monitor-alerts/main.tf
@@ -132,7 +132,7 @@ module "eks_blueprints" {
 }
 
 module "eks_blueprints_kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.21.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.32.0"
 
   eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
